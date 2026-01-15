8ae4e37671891623d90cb6d4eb975d71b1bbafda
luroblesf <rob23047@byui.edu>
2026-01-14 18:44:33 -0600

    assignment 2

---
+++ database/assignment2.sql
@@ -0,0 +1,34 @@
+INSERT INTO "account"(
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
+UPDATE "account"
+SET account_type = 'Admin'
+WHERE account_firstname = 'Tony'
+    AND account_lastname = 'Stark';
+DELETE FROM "account"
+WHERE account_firstname = 'Tony'
+    AND account_lastname = 'Stark';
+UPDATE "inventory"
+SET inv_description = REPLACE (
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
+WHERE inv_make = 'GM'
+    AND inv_model = 'Hummer';
+SELECT inv_make,
+    inv_model
+FROM inventory
+WHERE classification_id = '2';
+UPDATE "inventory"
+SET inv_image = REPLACE (inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE (inv_thumbnail, '/images/', '/images/vehicles/');
