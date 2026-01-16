d71bb2c7a4c1bea1a005309bd0306058e509070d
luroblesf <rob23047@byui.edu>
2026-01-15 19:48:02 -0600

    Assignment 2

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,3 +1,5 @@
+-- Assignment 2 SQL Script
+-- Add your SQL code here
 INSERT INTO "account"(
         account_firstname,
         account_lastname,
@@ -10,13 +12,16 @@ VALUES (
         'tony@starkent.com',
         'Iam1ronM@n'
     );
+-- Update account type to Admin for Tony Stark
 UPDATE "account"
 SET account_type = 'Admin'
 WHERE account_firstname = 'Tony'
     AND account_lastname = 'Stark';
+-- Delete Tony Stark from account table
 DELETE FROM "account"
 WHERE account_firstname = 'Tony'
     AND account_lastname = 'Stark';
+--  Update inv_description for GM Hummer
 UPDATE "inventory"
 SET inv_description = REPLACE (
         inv_description,
@@ -25,10 +30,12 @@ SET inv_description = REPLACE (
     )
 WHERE inv_make = 'GM'
     AND inv_model = 'Hummer';
+-- Select inv_make and inv_model for all 'Sport' classification vehicles
 SELECT inv_make,
     inv_model
 FROM inventory
 WHERE classification_id = '2';
+-- Update inv_image and inv_thumbnail paths to include 'vehicles' subdirectory
 UPDATE "inventory"
 SET inv_image = REPLACE (inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE (inv_thumbnail, '/images/', '/images/vehicles/');
