8d47d982aff5b671a51a29c0fc213bd81c968b76
luroblesf <rob23047@byui.edu>
2026-01-22 22:06:02 -0600

    try

--- server.js
+++ server.js
@@ -6,66 +6,63 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const expressLayouts = require("express-ejs-layouts")
-const utilities = require("./utilities")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const errorRoutes = require('./routes/errorRoutes')
-const errorHandler = require('./middleware/errorHandler')
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const expressLayouts = require("express-ejs-layouts");
+const utilities = require("./utilities");
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
+const errorRoutes = require('./routes/errorRoutes');
+const errorHandler = require('./middleware/errorHandler');
 const path = require("path");
-
 
 /* ***********************
  * View Engine and Views
  *************************/
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 /* ***********************
  * View Layouts
  *************************/
-app.use(expressLayouts)
-app.set("layout", "layouts/layout")
+app.use(expressLayouts);
+app.set("layout", "layouts/layout");
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
 // Inventory routes
-app.use("/inventory", inventoryRoute)
+app.use("/inventory", inventoryRoute);
 
 // Index route
-app.get("/", utilities.handleErrors(baseController.buildHome))
+app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Error Routes (intencional 500)
-app.use('/', errorRoutes)
+app.use('/', errorRoutes);
 
 // File Not Found Route - must be last route in list
 app.use((req, res, next) => {
-  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
-})
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
+});
 
 /* ***********************
  * Express Error Handler
  * Place after all other middleware
  *************************/
-app.use(errorHandler)
+app.use(errorHandler);
 
 /* ***********************
- * Local Server Information
- * Values from .env (environment) file
+ * Server Information
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
